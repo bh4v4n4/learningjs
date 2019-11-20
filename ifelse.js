1724112951abeca0cb95c45bf70bc0171b89ef96
unknown <bhavna.anand120@gmail.com>
2019-11-20 11:53:44 +0530

    my initial commit

---
+++ ifelse.js
@@ -0,0 +1,21 @@
+<html>
+   <body>     
+      <script type = "text/javascript">
+         <!--
+         function Warn(){
+            var age = 4;
+         
+            if( age > 18 ) {
+               document.write("<b>Qualifies for driving</b>");
+            }
+            else{
+                document.write("not qualified");
+            }
+         }
+         //-->
+      </script>      
+      <p>Set the variable to different value and then try...</p>
+   <input type = "button" value = "Click Me" onclick = "Warn();" /> 
+
+   </body>
+</html>
